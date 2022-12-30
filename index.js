ee3abe10769dc63c7fba08e49f1eaabab165e08d
anwarul <anwarulhoque032@gmail.com>
2022-12-30 23:25:08 +0600

    create basic server and added mongodb

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d73rjdi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('ema-john server running.....');
+})
+
+app.listen(port, () => {
+    console.log(`ema-john server running on ${port}`);
+})
