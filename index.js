c925d33c2f4eab879d972d84d60191f513e5e756
anwarul <anwarulhoque032@gmail.com>
2023-01-01 21:03:08 +0600

    added count products for pagination

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d73rjdi.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run () {
+    try{
+        const productCollection = client.db('emaJohn').collection('products');
+        
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productCollection.estimatedDocumentCount()
+            res.send( { count, products } )
+        });
+    }
+    finally {
 
+    }
+}
+run().catch(error => console.error(error));
 
 
 
